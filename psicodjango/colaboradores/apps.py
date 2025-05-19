14e6dcb6cd2917edc5815c5fafd377179766afd5
Igor Rodrigues <rodrigues.igor.ir@gmail.com>
2025-05-18 21:53:57 -0300

    Refactor app names to include full module paths for colaboradores, empresas, and grupos

--- psicodjango/colaboradores/apps.py
+++ psicodjango/colaboradores/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class ColaboradoresConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'colaboradores'
+    name = 'psicodjango.colaboradores'
 
     def ready(self):
         import colaboradores.signals
--- psicodjango/grupos/apps.py
+++ psicodjango/grupos/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class GruposConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'grupos'
+    name = 'psicodjango.grupos'
